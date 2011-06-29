66f75760a90c3a1f45def2f286905dad6e74775a
David Costa <david@zarel.net>
2011-06-29 11:58:52 +0000

    (Calc2) Aggiunta una modalità nello script di dev. Aggiunto l'abbozzo della procedura di attesa input utente. La funzione BOX_P disegna il frame solo se necessario.
    git-svn-id: svn+ssh://svn.zarel.net/repo@147 c7656a53-6912-42cc-9828-6bc42c15c6a9

---
+++ user.asm
@@ -0,0 +1,37 @@
+; Interazione con l'utente
+
+CODE_S segment public 'code'
+	assume CS:CODE_S,DS:DATA_S,SS:STACK_S
+
+; QUIT_P esce dal programma con il valore di ritorno in exitCode
+QUIT_P proc near
+	mov AH,4Ch
+	mov AL,exitCode
+	int 21h
+QUIT_P endp
+
+USER_P proc near
+	push AX
+	mov AH,00h
+	int 16h ; routine BIOS attendi tasto
+	; AL=ASCII, AH=scancode
+	
+	;uscita dal programma
+	cmp AL,'q'
+	je quitChoice
+	cmp AL,'Q'
+	je quitChoice
+	;nessuna scelta, esco senza azioni
+	jmp quitUserP
+
+	quitChoice:
+		call QUIT_P
+	
+	jmp quitUserP
+
+	quitUserP:
+	pop AX
+	ret
+USER_P endp
+
+CODE_S ends
